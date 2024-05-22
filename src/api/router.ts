228da9e139a1638fddb08211381e53db5dd98a10
Leonardo <leonardo10sp@gmail.com>
2024-05-21 23:41:43 -0300

    feat: create project, setup typescript and endpoint's test

---
+++ src/api/router.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { getAnotherTest, getTest } from "./controller";
+// import { getAnotherTest, getTest } from '@/controller';
+
+export const router = express.Router();
+
+router.get("/test", getTest);
+router.get("/another-test", getAnotherTest);
