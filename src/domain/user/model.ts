8d2a5330eb564d9801ad2c29226ef4297fbcf566
Leonardo <leonardo10sp@gmail.com>
2024-05-22 21:01:12 -0300

    feat: add createUser and getUserByCPF endpoints

---
+++ src/domain/user/model.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - cpf
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the user
+ *         name:
+ *           type: string
+ *           description: The name of the user
+ *         email:
+ *           type: string
+ *           description: The email of the user
+ *         cpf:
+ *           type: string
+ *           description: The cpf of the user
+ *       example:
+ *         id: d5fE_asz,
+ *         name: Harry Potter and the Goblet of Fire,
+           email: emailtest123@gmail.com,
+           cpf: 00000000,
+ */
+
+const UserSchema = new mongoose.Schema({
+  id: { type: "String" },
+  name: { type: "String", required: true },
+  email: { type: "String", required: true },
+  cpf: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{11}$/.test(v); // Ensures the CPF is exactly 11 digits
+      },
+      message: (props) => `${props.value} is not a valid CPF number!`,
+    },
+  },
+});
+
+const users = mongoose.model("users", UserSchema);
+
+export default users;
--- src/presentation/http/routes/index.ts
+++ src/presentation/http/routes/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import products from "./product/routes";
+import users from "./user/routes";
 
 const routes = (app: any) => {
   app.route("/").get((req: any, res: any) => {
     res.status(200).send({ title: "Node Course" });
   }),
-    app.use(express.json(), products);
+    app.use(express.json(), products, users);
 };
 
 export default routes;
