d0291360cdd51445d46c8180501024a50667bda6
Leonardo <leonardo10sp@gmail.com>
2024-05-23 20:42:54 -0300

    fix: update product endpoint

--- src/adapters/controllers/ProductController.ts
+++ src/adapters/controllers/ProductController.ts
@@ -1,34 +1,73 @@
 import express, { Request, Response } from "express";
+import fs from "fs";
+import multer from "multer";
+import products from "../../domain/model/ProductModel";
 import ProductService from "../../domain/service/ProductService";
 
 const router = express.Router();
 
+const upload = multer({ dest: "uploads/" });
+
 /*[APIS PRODUTO]*/
 class ProductController {
-
-
   /*[CRIAR PRODUTO]*/
   static createProduct = async (req: Request, res: Response) => {
+    const { name, category, price, description } = req.body;
+    const imagePath = req?.file?.path || "";
+
     try {
-      const savedProduct = await ProductService.createProduct(req.body);
-      res.status(201).send(savedProduct);
-    } catch (err : any) {
-      res.status(500).send({ message: err.message });
+      const imageBase64 = fs.readFileSync(imagePath, { encoding: "base64" });
+      const imageDataUri = `data:${req?.file?.mimetype};base64,${imageBase64}`;
+
+      const newProduct = new products({
+        name,
+        category,
+        price,
+        description,
+        image: imageDataUri,
+      });
+
+      await newProduct.save();
+
+      fs.unlinkSync(imagePath);
+
+      res.status(201).send(newProduct);
+    } catch (error: any) {
+      res.status(500).send({ message: error?.message });
     }
   };
-
 
   /*[ATUALIZAR PRODUTO]*/
   static updateProduct = async (req: Request, res: Response) => {
     const { id } = req.params;
+    const { name, category, price, description } = req.body;
+    const imagePath = req?.file?.path || "";
+
     try {
-      const message = await ProductService.updateProduct(id, req.body);
-      res.status(200).send(message);
-    } catch (err : any) {
-      res.status(500).send({ message: err.message });
+      const product = await products.findById(id);
+      if (!product) {
+        return res.status(404).send({ message: "Product not found" });
+      }
+
+      if (imagePath) {
+        const imageBase64 = fs.readFileSync(imagePath, { encoding: "base64" });
+        const imageDataUri = `data:${req?.file?.mimetype};base64,${imageBase64}`;
+        product.image = imageDataUri;
+        fs.unlinkSync(imagePath);
+      }
+
+      product.name = name || product.name;
+      product.category = category || product.category;
+      product.price = price || product.price;
+      product.description = description || product.description;
+
+      await product.save();
+
+      res.status(200).send(product);
+    } catch (error: any) {
+      res.status(500).send({ message: error?.message });
     }
   };
-
 
   /*[DELETAR PRODUTO]*/
   static deleteProduct = async (req: Request, res: Response) => {
@@ -36,19 +75,20 @@ class ProductController {
     try {
       const message = await ProductService.deleteProduct(id);
       res.status(200).send(message);
-    } catch (err : any) {
+    } catch (err: any) {
       res.status(500).send({ message: err.message });
     }
   };
 
-
-/*[LISTAR PRODUTOS POR CATEGORIA PRODUTO]*/
+  /*[LISTAR PRODUTOS POR CATEGORIA PRODUTO]*/
   static listProductByCategory = async (req: Request, res: Response) => {
     const { category } = req.params;
     try {
-      const productsByCategory = await ProductService.listProductsByCategory(category);
+      const productsByCategory = await ProductService.listProductsByCategory(
+        category
+      );
       res.status(200).send(productsByCategory);
-    } catch (err : any) {
+    } catch (err: any) {
       res.status(500).send({ message: err.message });
     }
   };
@@ -56,8 +96,16 @@ class ProductController {
 
 /*[DEFININDO ENDPOINTS DE PRODUTO]*/
 router.get("/products/:category", ProductController.listProductByCategory);
-router.post("/products", ProductController.createProduct);
-router.put("/products/:id", ProductController.updateProduct);
+router.post(
+  "/products",
+  upload.single("image"),
+  ProductController.createProduct
+);
+router.put(
+  "/products/:id",
+  upload.single("image"),
+  ProductController.updateProduct
+);
 router.delete("/products/:id", ProductController.deleteProduct);
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,9 @@ import users from "../adapters/controllers/UserController";
 
 const routes = (app: any) => {
   // Definição da rota raiz com método GET
-  app.route("/")
-    .get((req: any, res: any) => {
-      res.status(200).send({ title: "Node Course" });
-    });
+  app.route("/").get((req: any, res: any) => {
+    res.status(200).send({ title: "Tech Challenge" });
+  });
 
   // Definição das rotas de produtos e usuários
   app.use(express.json(), products, users);
