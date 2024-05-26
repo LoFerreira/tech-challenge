550089b245cf41a8ed492384068185d6e100f245
Leonardo <leonardo10sp@gmail.com>
2024-05-26 13:34:45 -0300

    feat: add integration to dockerfile and create .env file

--- src/infrastructure/database/MongoDB.ts
+++ src/infrastructure/database/MongoDB.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
-const uri =
-  "mongodb+srv://leonardo10sp:atmgdtdt66@alura.14fliwi.mongodb.net/alura-node?";
+const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?`;
 
 mongoose.connect(uri);
 
