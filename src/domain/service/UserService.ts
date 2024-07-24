c34632f056169522209ce47d813ea4d422c7f397
Leonardo <leonardo10sp@gmail.com>
2024-07-24 20:11:13 -0300

    fix: create user swagger file

--- src/domain/service/UserService.ts
+++ src/domain/service/UserService.ts
@@ -1,29 +1,25 @@
 import User from "../model/UserModel"; // Corrigindo o import aqui
 
-
 class UserService {
-
-    /*[CRIAR USUÁRIO]*/
-    static async createUser(userData: any) {
-        try {
-            let user = new User(userData); // Corrigindo o uso de 'User' aqui
-            const savedUser = await user.save();
-            return savedUser.toJSON();
-        }
-        catch (err: any) {
-            throw new Error(`Failure to register user: ${err.message}`);
-        }
+  /*[CRIAR USUÁRIO]*/
+  static async createUser(userData: any) {
+    try {
+      let user = new User(userData);
+      const savedUser = await user.save();
+      return savedUser.toJSON();
+    } catch (err: any) {
+      throw new Error(`Failure to register user: ${err.message}`);
     }
+  }
 
-    /*[BUSCA USUÁRIO COM BASE NO CPF]*/
-    static async getUserByCPF(cpf: string) {
-        try {
-            return await User.find({ cpf }, {});
-        }
-        catch (err: any) {
-            throw new Error(`Error retrieving user by CPF: ${err.message}`);
-        }
+  /*[BUSCA USUÁRIO COM BASE NO CPF]*/
+  static async getUserByCPF(cpf: string) {
+    try {
+      return await User.find({ cpf }, {});
+    } catch (err: any) {
+      throw new Error(`Error retrieving user by CPF: ${err.message}`);
     }
+  }
 }
 
 export default UserService;
