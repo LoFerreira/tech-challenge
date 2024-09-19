37044585bb4d2c616661411ff13d5762e9dc7a88
Leonardo <leonardo10sp@gmail.com>
2024-09-18 22:34:28 -0300

    fix: mongodb response

--- src/external/database/mongoDB/frameworks/mongoose/models/OrderModel.ts
+++ src/external/database/mongoDB/frameworks/mongoose/models/OrderModel.ts
@@ -1,39 +1,56 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Document, Schema } from "mongoose";
 
 // Define possíveis valores para status e pagamento
-const ORDER_STATUSES = ['OPENED', 'RECEIVED', 'PREPARING', 'DONE', 'FINISHED', 'CANCELED'] as const;
-const PAYMENT_STATUSES = ['PENDING', 'PROCESSING', 'PAID', 'UNPAID', 'REJECTED'] as const;
+const ORDER_STATUSES = [
+  "OPENED",
+  "RECEIVED",
+  "PREPARING",
+  "DONE",
+  "FINISHED",
+  "CANCELED",
+] as const;
+export const PAYMENT_STATUSES = [
+  "PENDING",
+  "PROCESSING",
+  "PAID",
+  "UNPAID",
+  "REJECTED",
+] as const;
 
 // Define a interface para o documento de pedido
 interface IOrder extends Document {
-    user: mongoose.Schema.Types.ObjectId;
-    status: typeof ORDER_STATUSES[number];
-    orderProducts: Array<{
-        product: mongoose.Schema.Types.ObjectId;
-        quantity: number;
-        price: number;
-    }>;
-    createdAt: Date;
-    payment: typeof PAYMENT_STATUSES[number];
-    totalAmount: number;
+  user: mongoose.Schema.Types.ObjectId;
+  status: (typeof ORDER_STATUSES)[number];
+  orderProducts: Array<{
+    product: mongoose.Schema.Types.ObjectId;
+    quantity: number;
+    price: number;
+  }>;
+  createdAt: Date;
+  payment: (typeof PAYMENT_STATUSES)[number];
+  totalAmount: number;
 }
 
 // Define o esquema Mongoose para a coleção de pedidos
 const OrderSchema: Schema = new Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    status: { type: String, required: true, enum: ORDER_STATUSES },
-    orderProducts: [
-        {
-            product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-            quantity: { type: Number, required: true },
-            price: { type: Number, required: true }
-        }
-    ],
-    createdAt: { type: Date, default: Date.now },
-    payment: { type: String, required: true, enum: PAYMENT_STATUSES },
-    totalAmount: { type: Number, required: true }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  status: { type: String, required: true, enum: ORDER_STATUSES },
+  orderProducts: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: { type: Number, required: true },
+      price: { type: Number, required: true },
+    },
+  ],
+  createdAt: { type: Date, default: Date.now },
+  payment: { type: String, required: true, enum: PAYMENT_STATUSES },
+  totalAmount: { type: Number, required: true },
 });
 
 // Cria e exporta o modelo Mongoose
-const OrderModel = mongoose.model<IOrder>('Order', OrderSchema);
+const OrderModel = mongoose.model<IOrder>("Order", OrderSchema);
 export default OrderModel;
